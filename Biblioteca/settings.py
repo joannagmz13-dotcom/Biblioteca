efc7c8e79d875930daa92647b36bf12920e98a84
Joanna Flores <joanna.flores@gmail.com>
2025-12-06 14:41:33 -0600

    primero3

--- Biblioteca/settings.py
+++ Biblioteca/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,6 +137,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Esta línea le indica a Django que busque los archivos estáticos en la carpeta "static"
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # recolecta todo lo estatic
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn Biblioteca.wsgi
+web:  python manage.py collectstatic && gunicorn Biblioteca.wsgi
